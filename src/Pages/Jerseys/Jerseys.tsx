219ddc09d3c1b78f017aeb4120a7b1f2e07a4643
abdalla980 <100001276@stud.srh-university.de>
2025-08-22 07:31:05 +0400

    names

--- src/Pages/Jerseys/Jerseys.tsx
+++ src/Pages/Jerseys/Jerseys.tsx
@@ -25,42 +25,42 @@ export const Jerseys = () => {
             <div className={styles.gridContainer}>
                 <div className={styles.one}>
                     <img onClick={Goto1891} src="1891shirt.jpg" className={styles.oneimg} />
-                    <h2>Nike 1891 Black edition</h2>
+                    <h2>"SmokingLirare" Black</h2>
                     <button className={styles.viewButton} onClick={Goto1891}>View</button>
                 </div>
                 <div className={styles.one}>
                     <img onClick={GotoBlackBarca} src="black barca shirt .jpg" className={styles.oneimg} />
-                    <h2>Black barca shirt</h2>
+                    <h2>Black & White Special Barcelona </h2>
                     <button className={styles.viewButton} onClick={GotoBlackBarca}>View</button>
                 </div>
                 <div className={styles.one}>
                     <img onClick={GotoBlackPalestine} src="Blackpalestineshirt.jpg" className={styles.oneimg} />
-                    <h2>Black palestine shirt</h2>
+                    <h2>Maillot Palestine Black 24-25</h2>
                     <button className={styles.viewButton} onClick={GotoBlackPalestine}>View</button>
                 </div>
                 <div className={styles.one}>
                     <img onClick={GotoEngland} src="Englandshirt.jpg" className={styles.oneimg} />
-                    <h2>England shirt</h2>
+                    <h2>England Black Special kit</h2>
                     <button className={styles.Englandviewbutton} onClick={GotoEngland}>View</button>
                 </div>
                 <div className={styles.one}>
                     <img onClick={GotoMiami} src="Miamishirt.jpg" className={styles.oneimg} />
-                    <h2>Miami shirt</h2>
+                    <h2>Miami E-special kit 24-25</h2>
                     <button className={styles.Englandviewbutton} onClick={GotoMiami}>View</button>
                 </div>
                 <div className={styles.one}>
                     <img onClick={GotoPinkPalestine} src="pinkpalestineshirt.jpg" className={styles.oneimg} />
-                    <h2>pink palestine shirt</h2>
+                    <h2>Maillot Palestine Black 24-25</h2>
                     <button className={styles.viewButton} onClick={GotoPinkPalestine}>View</button>
                 </div>
                 <div className={styles.one}>
                     <img onClick={GotoPSG} src="psg shirt .jpg" className={styles.oneimg} />
-                    <h2>psg shirt</h2>
+                    <h2>PSG 'Paris Noir' Special</h2>
                     <button className={styles.viewButton} onClick={GotoPSG}>View</button>
                 </div>
                 <div className={styles.one}>
                     <img onClick={GotoSantos} src="santos shirt.jpg" className={styles.oneimg} />
-                    <h2>Santos Shirt</h2>
+                    <h2>Santos Away Fan Edition</h2>
                     <button className={styles.viewButton} onClick={GotoSantos}>View</button>
                 </div>
             </div>
